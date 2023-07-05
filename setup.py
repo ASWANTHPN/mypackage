d26f63fd5f89a4951174ed691269ec0ba1df77f4
aswant <aswant.pn@bdb.ai>
2023-07-05 12:10:45 +0530

    initialcommit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-operations',
+    version='0.1.0',
+    description='A simple Python package for basic math operations',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
